d70741dc1ba766fb5ffb67da0683349943c8bc8f
Marcin Tomaszewski <marcin.e.tomaszewski@gmail.com>
2023-10-24 00:02:09 +0200

    Define contract for stack

---
+++ src/main/java/org/home/algorithms/stack/Stack.java
@@ -0,0 +1,11 @@
+package org.home.algorithms.stack;
+
+public interface Stack<E> extends Iterable<E> {
+
+    boolean isEmpty();
+
+    void push(E item);
+
+    E pop() throws IllegalStateException;
+
+}
