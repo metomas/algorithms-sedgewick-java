cabbf7fed7dfc204b11e13260bb246f0e69b08b4
Marcin Tomaszewski <marcin.e.tomaszewski@gmail.com>
2023-10-24 23:12:02 +0200

    Implement stack using array with fixed capacity

---
+++ src/main/java/org/home/algorithms/stack/FixedCapacityArrayStack.java
@@ -0,0 +1,59 @@
+package org.home.algorithms.stack;
+
+import java.util.Iterator;
+
+public class FixedCapacityArrayStack<E> implements Stack<E> {
+
+    private final E[] stack;
+    private int n = 0;
+
+    @SuppressWarnings("unchecked")
+    public FixedCapacityArrayStack(int capacity) {
+        this.stack = (E[]) new Object[capacity];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    @Override
+    public void push(E item) {
+        if (n == stack.length) {
+            throw new IllegalStateException("Full stack");
+        }
+
+        stack[n++] = item;
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Empty stack");
+        }
+
+        E item = stack[--n];
+        stack[n] = null; // avoid loitering to allow GC reclaim memory
+        return item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<E> {
+
+        private int i = n;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public E next() {
+            return stack[--i];
+        }
+    }
+}
---
+++ src/test/java/org/home/algorithms/stack/FixedCapacityArrayStackTest.java
@@ -0,0 +1,38 @@
+package org.home.algorithms.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatException;
+
+class FixedCapacityArrayStackTest implements StackContract<String, Stack<String>, List<String>> {
+
+    @Override
+    public Stack<String> createInstance() {
+        return new FixedCapacityArrayStack<>(createTestItems().size());
+    }
+
+    @Override
+    public List<String> createTestItems() {
+        return List.of("a", "b", "c");
+    }
+
+    @Test
+    public void testUnderflow() {
+        assertThatException()
+                .isThrownBy(createInstance()::pop)
+                .isInstanceOf(IllegalStateException.class)
+                .withMessage("Empty stack");
+    }
+
+    @Test
+    void testOverflow() {
+        Stack<String> stack = new FixedCapacityArrayStack<>(0);
+
+        assertThatException()
+                .isThrownBy(() -> stack.push("a"))
+                .isInstanceOf(IllegalStateException.class)
+                .withMessage("Full stack");
+    }
+}
