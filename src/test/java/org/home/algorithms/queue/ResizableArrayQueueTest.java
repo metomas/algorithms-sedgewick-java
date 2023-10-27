200634d29818f4057953b3dfcac63a3bdd76cc74
Marcin Tomaszewski <marcin.e.tomaszewski@gmail.com>
2023-10-27 23:51:16 +0200

    Test resetting the head and tail after resizing the queue

--- src/test/java/org/home/algorithms/queue/ResizableArrayQueueTest.java
+++ src/test/java/org/home/algorithms/queue/ResizableArrayQueueTest.java
@@ -2,9 +2,11 @@ package org.home.algorithms.queue;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ResizableArrayQueueTest implements QueueContract<String, Queue<String>, List<String>> {
 
@@ -24,6 +26,34 @@ class ResizableArrayQueueTest implements QueueContract<String, Queue<String>, Li
                 .isThrownBy(createInstance()::dequeue)
                 .isInstanceOf(IllegalStateException.class)
                 .withMessage("Empty queue");
+    }
+
+    @Test
+    void resettingHeadAndTailAfterResizing() {
+        var queue = createInstance();
+        var items = List.of("a", "b", "c");
+        var expected = new LinkedList<String>();
+
+        // enqueue all items
+        for (String item : items) {
+            queue.enqueue(item);
+            expected.add(item);
+        }
+
+        // dequeue a single item
+        assertEquals(items.get(0), queue.dequeue());
+        expected.removeFirst();
+
+        // enqueue another two items
+        for (String item : List.of("d", "e")) {
+            queue.enqueue(item);
+            expected.add(item);
+        }
+
+        // dequeue all items
+        for (String item : expected) {
+            assertEquals(item, queue.dequeue());
+        }
     }
 
 }
