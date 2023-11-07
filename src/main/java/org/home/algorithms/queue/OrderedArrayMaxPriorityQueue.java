c6a5be03793fbdaf2c713b0bc94185e1c660dac0
Marcin Tomaszewski <marcin.e.tomaszewski@gmail.com>
2023-11-08 00:15:46 +0100

    Implement priority queue using ordered array

---
+++ src/main/java/org/home/algorithms/queue/OrderedArrayMaxPriorityQueue.java
@@ -0,0 +1,75 @@
+package org.home.algorithms.queue;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Time complexity
+ * <ul>
+ *     <li>insert: O(N)</li>
+ *     <li>delete max: O(1)</li>
+ *     <li>max: O(1)</li>
+ * </ul>
+ */
+public class OrderedArrayMaxPriorityQueue<E extends Comparable<E>> implements MaxPriorityQueue<E> {
+
+    private static final int DEFAULT_CAPACITY = 8;
+
+    private E[] queue;
+    private int n = 0;
+
+    @SuppressWarnings("unchecked")
+    public OrderedArrayMaxPriorityQueue() {
+        this.queue = (E[]) new Comparable[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public void insert(E item) {
+        if (n == queue.length) resize(2 * n);
+        queue[n++] = item;
+        sort();
+    }
+
+    @Override
+    public E max() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return queue[n - 1];
+    }
+
+    @Override
+    public E deleteMax() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+
+        E item = queue[--n];
+        queue[n] = null;
+
+        if (n > 0 && n == queue.length / 4) resize(queue.length / 2);
+
+        return item;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    private void sort() {
+        for (int i = n - 1; i > 0 && less(queue[i], queue[i - 1]); i--) swap(i, i - 1);
+    }
+
+    private void resize(int size) {
+        @SuppressWarnings("unchecked")
+        E[] copy = (E[]) new Comparable[size];
+        if (n >= 0) System.arraycopy(queue, 0, copy, 0, n);
+        queue = copy;
+    }
+
+    private <T extends Comparable<T>> boolean less(T t, T t2) {
+        return t.compareTo(t2) < 0;
+    }
+
+    private void swap(int i, int j) {
+        E aux = queue[i];
+        queue[i] = queue[j];
+        queue[j] = aux;
+    }
+}
---
+++ src/test/java/org/home/algorithms/queue/OrderedArrayMaxPriorityQueueTest.java
@@ -0,0 +1,39 @@
+package org.home.algorithms.queue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThatException;
+
+class OrderedArrayMaxPriorityQueueTest
+        implements MaxPriorityQueueContract<String, MaxPriorityQueue<String>, List<String>> {
+
+    @Override
+    public MaxPriorityQueue<String> createInstance() {
+        return new OrderedArrayMaxPriorityQueue<>();
+    }
+
+    @Override
+    public List<String> createTestItems() {
+        return List.of("a", "d", "c", "b");
+    }
+
+    @Test
+    public void maxUnderflow() {
+        assertThatException()
+                .isThrownBy(createInstance()::max)
+                .isInstanceOf(NoSuchElementException.class)
+                .withMessage("Queue underflow");
+    }
+
+    @Test
+    public void deleteMaxUnderflow() {
+        assertThatException()
+                .isThrownBy(createInstance()::deleteMax)
+                .isInstanceOf(NoSuchElementException.class)
+                .withMessage("Queue underflow");
+    }
+
+}
